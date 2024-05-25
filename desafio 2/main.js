97faae15107904d9e00472f539b153d1224d4a39
Moretrap <traipecandela@gmail.com>
2024-05-25 01:21:49 -0300

    desafio 2

---
+++ desafio 2/main.js
@@ -0,0 +1,9 @@
+let NombreUsuario1 = prompt("ingrese su nombre");
+let NombreProducto1= 14
+let precio= 5
+
+Numero1 = 16
+
+prompt("cuantos articulos quiere comprar?")
+
+console.log("tu nombre es:" ,NombreUsuario1);
